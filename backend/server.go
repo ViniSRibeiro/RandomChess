b9c7adb1f5b0247245c542ae36688f84897e981d
JBradachi <105111795+JBradachi@users.noreply.github.com>
2025-06-24 14:04:25 -0300

    Feat:  inicio da rota de login

--- backend/server.go
+++ backend/server.go
@@ -8,9 +8,27 @@ import (
 
 func main() {
 	http.HandleFunc("/teste", teste)
+	http.HandleFunc("/login", s.login)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
 func teste(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "Olá, mundo!")
+}
+
+func (s *server) login(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "Metodo não permitido", http.StatusMethodNotAllowed)
+	}
+
+	err := r.ParseForm()
+	if err != nil {
+		http.Error(w, "Falha no parse do form", http.StatusInternalServerError)
+		return
+	}
+
+	usuario := r.FormValue("usuario")
+	senha := r.FormValue("senha")
+
+	res := s.db.Query
 }
