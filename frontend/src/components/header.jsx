ce5fdb9589271480409cde29b3dff5b00870aacb
Guilherme Broedel Zorzal <guilhermezorzal2004@gmail.com>
2025-06-25 02:20:36 -0300

    feat: adicionado borda no header é preciso ver se vai manter ou nao

--- frontend/src/components/header.jsx
+++ frontend/src/components/header.jsx
@@ -9,7 +9,7 @@ const Header = () => {
   return (
     <div className={styles.container}>
       <Link className={styles.title} to="/">RandomChess</Link>
-      <div className={styles.container}>
+      <div className={styles.pages}>
         {!isLogged ? (
           <>
             <Link className={styles.link} to="/cadastro">Cadastro</Link>
