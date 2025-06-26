471d012aa244187b2ad797c359be2c2c34048c78
Guilherme Broedel Zorzal <guilhermezorzal2004@gmail.com>
2025-06-26 11:13:29 -0300

    feat: rota de perfil

--- frontend/src/pages/perfil.jsx
+++ frontend/src/pages/perfil.jsx
@@ -1,9 +1,56 @@
-import React from "react"
+import React, { useState } from "react"
 
 const Perfil = () => {
+
+
+  const cadastro = (data) => {
+
+    const [nome, setNome] = useState("")
+    const [totPartidasJogadas, setTotPartidasJogadas] = useState("")
+    const [totVitorias, setTotVitorias] = useState("")
+    const [totEmpates, setTotEmpates] = useState("")
+    const [totDerrotas, setTotDerrotas] = useState("")
+
+    console.log(data)
+    fetch("http://localhost:8080/perfil", {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authentication': localStorage.getItem('token')
+      },
+    })
+      .then((response) => {
+        if (!response.ok) {
+          // Primeiramente é tentado recuperar os campos da resposta que indicam que deu errado. Se não conseguir, trata como um erro genérico
+          const obj = response.json()
+            .then((data) => {
+              if (data.hasOwnProperty('mensagem')) {
+                alert(data.mensagem)
+                return
+              }
+              else {
+                throw response
+              }
+            })
+            .catch((error) => {
+              console.log("cadastro.jsx >>> ", error)
+              return
+            })
+          return
+        }
+        setTimeout(2000)
+        alert("RECEBEU DATA ")
+      })
+      .catch((error) => {
+        alert("um erro inesperado ocorreu ao cadastrar o professor");
+        console.log("cadastro.jsx >>> ", error)
+        throw new error
+      })
+  }
+
   return (
     <div>
-      <h1>Perfil</h1>
+      <p>ALSKJLFF</p>
     </div>
   )
 }
